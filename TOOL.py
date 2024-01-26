69d6ce0a2c5ab658ec09fd18c65f163148d68b5e
David Schumacher <david_schumacher@t-online.de>
2024-01-26 19:15:49 +0100

    sync

--- TOOL.py
+++ TOOL.py
@@ -26,9 +26,6 @@ st.set_page_config(
     page_title="Stützen-Stütze",
     layout="wide",
     page_icon=(icon),
-    menu_items={
-        "github repository": "https://github.com/schudave/ba_trako",
-    }
 )
 #header
 spalte_titel, spalte_logo = st.columns([2, 0.5])
